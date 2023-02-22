1a13e582bc06988884e1a01de699c382c31f9128
Kishan-Gajnotar <kishangajnotar80@gmail.com>
2023-02-22 11:27:59 +0530

    modified:   FreeRTOSinit.py

--- FreeRTOSinit.py
+++ FreeRTOSinit.py
@@ -8,20 +8,28 @@ from canRx1 import can1
 from canRx2 import can2
 can1Rx = None 
 can2Rx = None
-t1
+chargingStationSanityTask = None
+chargerLoop = None
+telemetryParser = None
+type1Task = None
+idleTask = None
+ledTask = None
+energyMeterTask = None
+starkTXCallback = None
+
 
 def createTasks():
     global can1Rx, can2Rx
     can1Rx = threading.Thread(target =can1)
     can2Rx = threading.Thread(target =can2)
-    t1=threading.Thread(target=chargingStationTest.chargingStationSanityTask,args=())
-    t2=threading.Thread(target=chargingStationMain.chargerLoop,args=())
-    t3=threading.Thread(target=telemetryDevice.telemetryParser,args=())
-    # t4=threading.Thread(target=type1Task,args=())
-    # t5=threading.Thread(target=idleTask,args=())
-    # t6=threading.Thread(target=ledTask,args=())
-    t7=threading.Thread(target=energyMeter.energyMeterTask,args=())
-    t8=threading.Thread(target=flashCharger.starkTXCallback,args=())
+    chargingStationSanityTask=threading.Thread(target=chargingStationTest.chargingStationSanityTask,args=())
+    chargerLoop=threading.Thread(target=chargingStationMain.chargerLoop,args=())
+    telemetryParser=threading.Thread(target=telemetryDevice.telemetryParser,args=())
+    # type1Task=threading.Thread(target=type1Task,args=())
+    # idleTask=threading.Thread(target=idleTask,args=())
+    # ledTask=threading.Thread(target=ledTask,args=())
+    energyMeterTask=threading.Thread(target=energyMeter.energyMeterTask,args=())
+    starkTXCallback=threading.Thread(target=flashCharger.starkTXCallback,args=())
 
 
 def createEventGroups():
@@ -34,14 +42,14 @@ def vTaskStartScheduler():
 def stask():
     can1Rx.start()
     can2Rx.start()
-    t1.start()
-    t2.start()
-    t3.start()
-    # t4.start()
-    # t5.start()
-    # t6.start()
-    t7.start()
-    t8.start()
+    chargingStationSanityTask.start()
+    chargerLoop.start()
+    telemetryParser.start()
+    # type1Task.start()
+    # idleTask.start()
+    # ledTask.start()
+    energyMeterTask.start()
+    starkTXCallback.start()
 
     
 
