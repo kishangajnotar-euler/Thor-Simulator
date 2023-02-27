2bb2fff17ca8a1c66066520e077f0f5f61f4ef2d
SakshamEuler <saksham.arora@eulermotors.com>
2023-02-27 16:06:56 +0530

    energyMeteradded

--- functions.py
+++ functions.py
@@ -5,6 +5,9 @@ from main import CAN_2 as bus
 import can
 import canID
 from structure import chargerState
+import pandas as pd
+energytaski = 0
+chargerFlag = False
 
 
 chargerCantask = None
@@ -43,7 +46,15 @@ def setTTFC():
     bus.send(message)
 
 def setEnergyConsumed():
-    energyConsumed_can=int(25.75)
+    df = pd.read_csv('energyMeter.csv')
+
+    if energytaski > len(energytaski):
+        energyConsumed_can=int(df.iloc[-1]['value']*100)
+        chargerFlag = True
+    else:
+        energyConsumed_can=int(df.iloc[energytaski]['value']*100)
+        energytaski = energytaski + 1
+
     buffer=[0]*8  
     buffer[0] = (energyConsumed_can & 0xFF)
     buffer[1] = (energyConsumed_can >> 8) & 0xFF
@@ -149,8 +160,7 @@ def handleChargingState():
             chargerCantask=threading.Thread(target=chargerCanTask)
             chargerCantask.start()
         time.sleep(0.1)
-        count = count + 1 
-        if count > countL:
+        if chargerFlag:
             print("changing state from charging to idle ========================================")
             chargerState.state = 5
             chargerCantask.join()
@@ -170,9 +180,7 @@ def chargerCanTask():
             bus.send(msg)
             break
         if deviceParams.chargerType == 1 :
-            rxBMSData=[0]*8
-            rxBMSData[2] = 3
-            rxBMSData[3] = 2
+            global rxBMSData
             msg = can.Message(arbitration_id=canID.tx_6k6_charger, data=rxBMSData,is_extended_id=True)
             bus.send(msg)
             time.sleep(1)
